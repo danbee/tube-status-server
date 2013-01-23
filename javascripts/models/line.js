a13103b9e477cb67d43331cbd7132c6a3be62049
Dan Barber <dan@new-bamboo.co.uk>
2013-01-23 15:27:56 +0000

    Add refresh button and nav bar.

--- javascripts/models/line.js
+++ javascripts/models/line.js
@@ -2,7 +2,11 @@ define(['backbone'], function(Backbone) {
 
   var Line = Backbone.Model.extend({
     initialize: function() {
-      this.set("status_class", this.get("status").replace(' ', '-'));
+      this.set_status();
+    },
+
+    set_status: function() {
+      this.set("status_class", this.get("status").replace(', ', ',').replace(' ', '-').replace(',', ' '));
     }
   });
 
