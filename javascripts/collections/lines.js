2248226cbdaa4d0213d44b4311933854d6337cfe
Dan Barber <dan@new-bamboo.co.uk>
2013-01-30 13:07:45 +0000

    Use actual data.

--- javascripts/collections/lines.js
+++ javascripts/collections/lines.js
@@ -2,7 +2,6 @@ define(['backbone', 'models/line'], function(Backbone, Line) {
 
   var LinesCollection = Backbone.Collection.extend({
     // added callback=? to force jsonp
-    //url: "http://api.tubeupdates.com/?method=get.status&callback=?",
     url: "http://tubefeed.herokuapp.com/now.json?callback=?",
     urls: {
       now: "http://tubefeed.herokuapp.com/now.json?callback=?",
@@ -10,10 +9,6 @@ define(['backbone', 'models/line'], function(Backbone, Line) {
     },
 
     model: Line,
-
-    parse: function(data) {
-      return data.response.lines;
-    }
   });
 
   return LinesCollection;
